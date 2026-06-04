b003c16c331061270314d8c761a059445d88832c
Muhammad Taha <ts4737076@gmail.com>
2026-06-05 02:43:17 +0500

    Add files via upload

---
+++ lab 2/code solution/task1.java
@@ -0,0 +1,12 @@
+public class task1 {
+public static void main(String[] args){
+System.out.println("////////////////\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\"); 
+System.out.println("**       Students Points      **");  
+System.out.println("////////////////\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\");
+System.out.println("Name     \tLab  Bonus  Total");  
+System.out.println("----     \t---  -----  -----");  
+System.out.println("Joe      \t43   7      50"); 
+System.out.println("William  \t50   8      58"); 
+System.out.println("Mary Sue \t39   10     49"); 
+}    
+}
